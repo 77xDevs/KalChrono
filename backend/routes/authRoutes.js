78204e4e53472c7588b5487a0686c34231289f4c
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-02 23:50:22 +0530

    Add studentRegistration, errorMessages and authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
-import express from 'express'
-import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js'
-
+import express from 'express';
+import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js';
+import { studentLoginController } from '../controllers/auth/studentLoginController.js'; 
 
 export const authRouter = express.Router();
 
 authRouter.post('/studentRegistration', studentRegistrationController.studentRegistration);
 
+authRouter.post('/studentLogin', studentLoginController);

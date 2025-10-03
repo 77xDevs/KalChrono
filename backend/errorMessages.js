1ca4cae4ca361b8f51874ad39e97cbf0fcdc4470
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-03 20:31:09 +0530

    Fix merge conflicts in server.js

--- backend/errorMessages.js
+++ backend/errorMessages.js
@@ -1,7 +1,4 @@
 export const BAD_REQUEST =  "Bad request. The details you entered are incorrect"
 export const PASSWORD_MISMATCH = "Your passwords does not match, please make sure they're same before confirming and registering"
-<<<<<<< HEAD
-export const INVALID_CREDENTIALS = "Invalid roll number or password.";
-=======
+export const INVALID_CREDENTIALS = "Invalid roll number or password."
 export const STUDENT_NOT_FOUND = "Student with the entered Roll Number not found, please check and try again"
->>>>>>> main

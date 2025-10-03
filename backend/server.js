928850d6aa7d708405c6cc8f79cd63e97dcbc902
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-03 15:07:31 +0530

    Add server

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,25 @@ import cors from 'cors';
 import supabase from "./supabase-client.js";
 import { authRouter } from './routes/authRoutes.js';
 
-
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(express.json());
 
 app.use(cors({
-    origin: `http://localhost:${PORT}`,
+    origin: "http://localhost:3000",   // frontend runs on port 3000 typically
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
-app.use('/auth', authRouter);
 
-// app.get("/", async (req, res) => {
-//     const { data, error } = await supabase.from("department").select("*");
-//     if(error) return res.status(400).json({ error: "Error in retrieving"})
-//     res.send(data);
-// })
+app.get("/", (req, res) => {
+    res.json({ message: "Attendance backend is running" });
+});
+
+
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

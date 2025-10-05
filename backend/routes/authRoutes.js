b111417c24bfd70f281f37f7110f7b986a1ca774
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-05 18:49:46 +0530

    Updated authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js';
+import express from 'express'
+import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js'
 
-import { teacherRegistrationController } from '../controllers/auth/teacherRegistrationController.js';
+import { teacherRegistrationController } from '../controllers/auth/teacherRegistrationController.js'
 
 export const authRouter = express.Router();
 
 authRouter.post('/studentRegistration', studentRegistrationController.studentRegistration);
-
 
 authRouter.post('/teacherRegistration', teacherRegistrationController.teacherRegistration);
 

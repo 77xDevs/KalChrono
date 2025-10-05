0d77549b689a58ad7d819d3542776242026af8d3
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-05 18:13:19 +0530

    Removed studentLoginController route to authRouter

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js';
-import { studentLoginController } from '../controllers/auth/studentLoginController.js'; 
+
 import { teacherRegistrationController } from '../controllers/auth/teacherRegistrationController.js';
 
 export const authRouter = express.Router();
 
 authRouter.post('/studentRegistration', studentRegistrationController.studentRegistration);
 
-authRouter.post('/studentLogin', studentLoginController);
 
 authRouter.post('/teacherRegistration', teacherRegistrationController.teacherRegistration);
 

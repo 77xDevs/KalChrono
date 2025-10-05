671c86508b9a69bc912587686d9e29f43fac4ebe
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-05 18:39:24 +0530

    Updated errorMessages.js

--- backend/errorMessages.js
+++ backend/errorMessages.js
@@ -1,6 +1,5 @@
 export const BAD_REQUEST =  "Bad request. The details you entered are incorrect"
 export const PASSWORD_MISMATCH = "Your passwords does not match, please make sure they're same before confirming and registering"
 
-export const INVALID_CREDENTIALS = "Invalid roll number or password."
 export const STUDENT_NOT_FOUND = "Student with the entered Roll Number not found, please check and try again"
 export const TEACHER_NOT_FOUND = "Teacher with the entered ID not found, please check and try again";

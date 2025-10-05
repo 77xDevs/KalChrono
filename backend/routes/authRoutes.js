c4091234ffeab5383697569bbc86a6749645d818
nithisha-adepu <adepunithisha48@gmail.com>
2025-10-05 16:34:44 +0530

    Added teacherRegistration route to authRouter

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js';
 import { studentLoginController } from '../controllers/auth/studentLoginController.js'; 
+import { teacherRegistrationController } from '../controllers/auth/teacherRegistrationController.js';
 
 export const authRouter = express.Router();
 
 authRouter.post('/studentRegistration', studentRegistrationController.studentRegistration);
 
 authRouter.post('/studentLogin', studentLoginController);
+
+authRouter.post('/teacherRegistration', teacherRegistrationController.teacherRegistration);
+

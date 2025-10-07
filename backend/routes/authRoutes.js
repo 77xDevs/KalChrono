3630af4cf2b8fc5466368e9f2d3fcab467fc8104
pujyank2006 <pujyank2006@gmail.com>
2025-10-07 22:03:28 +0530

    .

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js'
 
+import { studentRegistrationController } from '../controllers/auth/studentRegistrationController.js'
 import { teacherRegistrationController } from '../controllers/auth/teacherRegistrationController.js'
+import { studentLoginController } from '../controllers/auth/studentLoginController.js';
+
 
 export const authRouter = express.Router();
 
 authRouter.post('/studentRegistration', studentRegistrationController.studentRegistration);
-
 authRouter.post('/teacherRegistration', teacherRegistrationController.teacherRegistration);
 
+authRouter.post('/studentLogin', studentLoginController.studentLogin);
